782014c2cca378fdfe6e7145febb8f38e7ce8c37
Priyanka9907 <priyankaguruprakash2020@gmail.com>
2024-07-11 00:42:53 +0530

    URLtask

--- Day 6 filter/XMLHTTPreq.js
+++ Day 6 filter/XMLHTTPreq.js
@@ -4,53 +4,48 @@ var todoRequest = new XMLHttpRequest();
 todoRequest.onreadystatechange = function () {
   var data = JSON.parse(this.response);
   console.log(data);
+
  
-  
+ //console.log(todoRequest);
+
+//Get all the countries from Asia continent /region using Filter method
+let asiancountries = data.filter((element) => {
+    return(element.region == "Asia" || element.continent == "Asia")
+      
+});
+console.log(asiancountries);
+
+asiancountries.forEach((elements) => {
+console.log(elements.name.common);
+});
+//console.log(todoRequest);
+
+//Get all the countries with a population of less than 2 lakhs using Filter method
+let populations = data.filter((countries) => {
+  return countries.population < 200000;
+});
+console.log("countries with a population of less than 2 lakhs ", populations);
+
+//Print the following details name, capital, flag, using forEach method
+
+let nameCapital = data.forEach((elementss)=>{
+  console.log (elementss.name, elementss.capital, elementss.flags);
+ });
+//Print the total population of countries using reduce method
+
+let totalpopulation = data.reduce((carry,totalPopul)=>{
+  console.log("total population")
+  return carry + totalPopul.population
+},0);
+console.log(totalpopulation);
+//Print the country that uses US dollars as currency.
+let countryUSD = data.filter((element)=>{
+  return element.currencies && element.currencies.USD;
+});
+console.log("country that uses US dollars as currency",countryUSD)
 };
-
 todoRequest.open("GET", todosAPi); // GET, POST, PUT, DELETE, PATCH
-todoRequest.send();
-
-console.log(todoRequest);
-
-function filterAsianCountries(data = []) {
-if(data.continents === "Asia" || data.region === "Asia"){
-    return data.country;
-}
-}
-var output= data.filter(filterAsianCountries(data));
- console.log(output);
-
-function filterPopulation(data = [], population = 0) {
-    if(population < 200000){
-        return data.country;
-    }
-}
-var populationOutput = data.filter(filterPopulation(data, 200000));
-console.log(populationOutput);
+ todoRequest.send();
 
 
-
-data.forEach((data=[]))=>{
-    console.log(data.name);
-    console.log(data.capital);
-    console.log(data.flag);
-}
-
-function totalPopulation(carry,population){
-    return carry + population;
-}
-var totalpopulationOutput= data.reduce(totalPopulation(data,population));
-console.log(totalpopulationOutput);
-
-
-
-function usdCountry(data=[], USD){
-    if(currencies === "USD"){
-        return data.country;
-    }
-}
-var usdCountryoutput = data.filter(usdCountry);
-console.log(usdCountryoutput);
-
-}
+ 
